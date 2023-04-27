5afc55c1ada0323c43abf357b3ecff6d26d2fca8
AzaiezAbdessalem <azaiezabdessalem@gmail.com>
2023-04-27 13:55:01 +0200

    update

--- membre/src/main/java/com/membre/membre/MembreApplication.java
+++ membre/src/main/java/com/membre/membre/MembreApplication.java
@@ -8,8 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 //@EnableFeignClients
 public class MembreApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(MembreApplication.class, args);
+    public static void main(String[] args) {SpringApplication.run(MembreApplication.class, args);
     }
 
 }
