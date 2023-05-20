0d36a642d8615aa488fe3b238b1c13c4bad391e3
AzaiezAbdessalem <azaiezabdessalem@gmail.com>
2023-05-20 12:17:51 +0200

    config and eureka

--- membre/src/main/java/com/membre/membre/MembreApplication.java
+++ membre/src/main/java/com/membre/membre/MembreApplication.java
@@ -6,8 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-
+@EnableDiscoveryClient
 @SpringBootApplication
 //@EnableFeignClients
 public class MembreApplication implements CommandLineRunner {
