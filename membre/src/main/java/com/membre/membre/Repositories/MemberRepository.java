4af74617286c4d93645d29ed9b02b14155c525c0
chaabanerahma8 <chaabanerahma8@gmail.com>
2023-06-17 11:14:58 +0100

    ajouter des methodes

--- membre/src/main/java/com/membre/membre/Repositories/MemberRepository.java
+++ membre/src/main/java/com/membre/membre/Repositories/MemberRepository.java
@@ -3,9 +3,17 @@ package com.membre.membre.Repositories;
 import com.membre.membre.Entities.Member;
 import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Transactional
 @Repository
 public interface MemberRepository extends JpaRepository<Member,Long> {
+    @Query("SELECT m FROM Member m JOIN m.positions p WHERE p.name = :name")
+    List<Member> findByPositionName(@Param("name") String name);
+    @Query("SELECT COUNT(m) FROM Member m JOIN m.positions p WHERE p.name = 'Apprenant'")
+    long countByPositionNameApprenant();
 }
