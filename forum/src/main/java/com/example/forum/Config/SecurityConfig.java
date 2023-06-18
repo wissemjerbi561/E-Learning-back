c5a4e157918da3e5bf142e766f2056ea4d69235a
AzaiezAbdessalem <azaiezabdessalem@gmail.com>
2023-06-18 23:29:37 +0200

    gateway

---
+++ forum/src/main/java/com/example/forum/Config/SecurityConfig.java
@@ -0,0 +1,26 @@
+package com.example.forum.Config;
+
+import lombok.AllArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@AllArgsConstructor
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests()
+                .anyRequest()
+                .authenticated();
+        http
+                .csrf().disable()
+                .oauth2ResourceServer()
+                .jwt();        return http
+                .build();    }
+}
